f6958fcc2c937faa4d66c46aaabf3282233a2414
Rain Tüür <rain.tuur@gmail.com>
2023-01-15 17:00:07 +0200

    Initial commit

---
+++ src/test/java/ee/valiit/back_bank_27/BackBank27ApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_bank_27;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBank27ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
