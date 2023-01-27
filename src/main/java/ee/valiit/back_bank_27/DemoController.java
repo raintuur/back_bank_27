4da4ed819773e23442094320195dd075ae31f256
Kuldar Nei <kuldarnei@gmail.com>
2023-01-27 12:49:59 +0200

    BEB27- andmebaasi create-reset-create-import toimingute skriptid

---
+++ src/main/java/ee/valiit/back_bank_27/DemoController.java
@@ -0,0 +1,13 @@
+package ee.valiit.back_bank_27;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("/all/atm/city")
+    public String helloWorld() {
+        return "Helloworld";
+    }
+}
