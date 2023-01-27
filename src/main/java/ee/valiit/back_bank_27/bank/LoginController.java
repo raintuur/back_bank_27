a1fd29e540b6d54ece6cdf18329bf417597ce1c2
Rain Tüür <rain.tuur@gmail.com>
2023-01-27 16:08:22 +0200

    Added DB stuff

---
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginController.java
@@ -0,0 +1,21 @@
+package ee.valiit.back_bank_27.bank;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+
+    @GetMapping("/login")
+    @Operation(summary = "Selle teenusega saab rakendussse sisse logida")
+    public LoginResponse login(@RequestParam String username, @RequestParam String password) {
+        LoginResponse response = new LoginResponse();
+        response.setUserId(202);
+        response.setRoleType("admin");
+        return response;
+    }
+
+}
