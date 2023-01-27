138fe4822f8cff85563c2a9570970e1e10123b39
Janeli <janelikurm@gmail.com>
2023-01-27 16:58:50 +0200

    Working againg

---
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
@@ -0,0 +1,12 @@
+package ee.valiit.back_bank_27.bank;
+
+public class LoginService {
+    public LoginResponse login(String username, String password) {
+        //to be implemented
+
+
+        return null;
+
+    }
+
+}
