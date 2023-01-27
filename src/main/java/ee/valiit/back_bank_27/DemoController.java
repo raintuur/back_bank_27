1f2ba9a46122d859f1739cc13675a471a287b6d9
katrila <katrilattu@gmail.com>
2023-01-27 12:48:59 +0200

    Added DB stuff

---
+++ src/main/java/ee/valiit/back_bank_27/DemoController.java
@@ -0,0 +1,8 @@
+package ee.valiit.back_bank_27;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    public String helloWorld ()
+}
