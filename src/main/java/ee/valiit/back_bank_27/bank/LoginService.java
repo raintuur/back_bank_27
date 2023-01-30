6e3e3bbfa70af2f0a54bc4cd253b6d94c3600a2a
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-30 10:12:04 +0200

    even though it worked, I went back on my swagger version

--- src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
@@ -1,5 +1,8 @@
 package ee.valiit.back_bank_27.bank;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
     public LoginResponse login(String username, String password) {
