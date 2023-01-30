f9e1778cd9c149f92b000a3e1af409f50670c24e
Rain Tüür <rain.tuur@gmail.com>
2023-01-30 12:51:04 +0200

    Added DB stuff

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserMapperImpl.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserMapperImpl.java
@@ -1,6 +1,7 @@
 package ee.valiit.back_bank_27.domain.user;
 
 import ee.valiit.back_bank_27.bank.LoginResponse;
+import ee.valiit.back_bank_27.domain.user.role.Role;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,18 @@ public class UserMapperImpl implements UserMapper {
 
     @Override
     public LoginResponse toDto(User user) {
-        return null;
+        // TODO: võtame Getteritega vajalikud andmed ja paneme need Setteritega Dto objekti
+        Integer userId = user.getId();
+
+        Role role = user.getRole();
+
+        String roleType = role.getType();
+
+
+        LoginResponse response = new LoginResponse();
+        response.setUserId(userId);
+        response.setRoleType(roleType);
+
+        return response;
     }
 }
