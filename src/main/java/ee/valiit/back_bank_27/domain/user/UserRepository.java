3ef646ba68041df202c5bb0f86acb1740dae7b06
indrekjaadla <indrekjaadla@gmail.com>
2023-01-30 15:16:52 +0200

    Message.

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserRepository.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserRepository.java
@@ -1,10 +1,11 @@
 package ee.valiit.back_bank_27.domain.user;
 
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository {
+public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsernameAndPassword(String username, String password);
 
-    User findById(Integer userId);
+
+
 }
