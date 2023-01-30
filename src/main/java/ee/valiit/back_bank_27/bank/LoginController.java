46f153d83d0754c61b9a6bd12d887cd66ac3a21d
Heda <hedakalmus@gmail.com>
2023-01-30 11:57:21 +0200

    loginService, UserService

--- src/main/java/ee/valiit/back_bank_27/bank/LoginController.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginController.java
@@ -1,6 +1,7 @@
 package ee.valiit.back_bank_27.bank;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,14 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoginController {
 
-    @GetMapping("/login")
+    @Resource
+    private LoginService loginService;
 
+    @GetMapping("/login")
     @Operation(summary = "Selle teenusega saab rakendusse sisse logida")
     public LoginResponse login(@RequestParam String username, @RequestParam String password) {
-        LoginService loginService = new LoginService();
+        LoginResponse loginResponse = loginService.login(username, password);
 
 
-
-        return null;
+        return loginResponse;
     }
 }
--- src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
@@ -1,8 +1,19 @@
 package ee.valiit.back_bank_27.bank;
 
+import ee.valiit.back_bank_27.domain.user.User;
+import ee.valiit.back_bank_27.domain.user.UserService;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
+    @Resource
+    private UserService userService;
+
+
     public LoginResponse login(String username, String password) {
+        User user = userService.findUser(username, password);
         //        to be implemented
 
         return null;
