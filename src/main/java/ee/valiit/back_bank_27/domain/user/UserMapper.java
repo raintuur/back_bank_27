9c732a68bfab756e45e346afa1fd7d2230873a17
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-30 12:52:43 +0200

    before break, login and response - done

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserMapper.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserMapper.java
@@ -3,7 +3,6 @@ package ee.valiit.back_bank_27.domain.user;
 import ee.valiit.back_bank_27.bank.LoginResponse;
 
 public interface UserMapper {
-    LoginResponse userToLoginResponse(User user);
 
         LoginResponse toDto(User user);
 
