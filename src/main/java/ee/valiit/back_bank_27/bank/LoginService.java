ff723807502608abae5013d9ca590ddc4d96b32e
Rain Tüür <rain.tuur@gmail.com>
2023-01-30 15:14:24 +0200

    Added DB stuff

--- src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
@@ -1,13 +1,10 @@
 package ee.valiit.back_bank_27.bank;
 
 import ee.valiit.back_bank_27.domain.user.User;
-import ee.valiit.back_bank_27.domain.user.UserMapperImpl;
+import ee.valiit.back_bank_27.domain.user.UserMapper;
 import ee.valiit.back_bank_27.domain.user.UserService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class LoginService {
@@ -16,14 +13,12 @@ public class LoginService {
     private UserService userService;
 
     @Resource
-    private UserMapperImpl userMapper;
+    private UserMapper userMapper;
 
     public LoginResponse login(String username, String password) {
         User user = userService.findUser(username, password);
         LoginResponse response = userMapper.toDto(user);
         return response;
     }
-
-
 
 }
