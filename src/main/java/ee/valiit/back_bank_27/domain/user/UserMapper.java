bf3687995aa05ede62933e972b8e6342ad6d7714
katrila <katrilattu@gmail.com>
2023-01-30 14:35:34 +0200

    Added DB stuff

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserMapper.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserMapper.java
@@ -2,9 +2,14 @@ package ee.valiit.back_bank_27.domain.user;
 
 import ee.valiit.back_bank_27.bank.LoginResponse;
 
+import java.util.List;
+
 public interface UserMapper {
 
     LoginResponse toDto(User user);
+
+
+    List<LoginResponse> toDto(List<User> users);
 
 
 }
--- src/main/java/ee/valiit/back_bank_27/domain/user/UserMapperImpl.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserMapperImpl.java
@@ -1,13 +1,48 @@
 package ee.valiit.back_bank_27.domain.user;
 
 import ee.valiit.back_bank_27.bank.LoginResponse;
+import ee.valiit.back_bank_27.domain.user.role.Role;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class UserMapperImpl implements UserMapper {
 
     @Override
     public LoginResponse toDto(User user) {
-        return null;
+        //TODO: võtame Getteritega vajalikud andmed ja paneme need Setteritega Dto objekti
+
+        Integer userId = user.getId();
+
+        Role role = user.getRole();
+
+        String roleType = role.getType();
+
+        LoginResponse response = new LoginResponse();
+        response.setUserId(userId);
+        response.setRoleType(roleType);
+
+        return response;
+    }
+
+    @Override
+    public List<LoginResponse> toDto(List<User> users) {
+
+        List<LoginResponse> responses = new ArrayList<>();
+
+        for (User user : users) {
+
+            responses.add(toDto(user));
+
+        }
+
+
+        return responses;
+    }
+
+    private static List<User> getUsers(List<User> users) {
+        return users;
     }
 }
