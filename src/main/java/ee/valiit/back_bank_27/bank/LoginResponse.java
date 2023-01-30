e849ab87b4544f6901a2ea4fd40bf006010c0475
Juhannes <pstimees@gmail.com>
2023-01-30 15:17:24 +0200

    Läbi Logini saab UserId kasutades username ja password'i

--- src/main/java/ee/valiit/back_bank_27/bank/LoginResponse.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginResponse.java
@@ -1,10 +1,12 @@
 package ee.valiit.back_bank_27.bank;
 
-import lombok.*;
+import lombok.Data;
 
 @Data
+
 public class LoginResponse {
     private Integer userId;
     private String roleType;
+
 
 }
---
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserMapper.java
@@ -0,0 +1,16 @@
+package ee.valiit.back_bank_27.domain.user;
+
+import ee.valiit.back_bank_27.bank.LoginResponse;
+import org.mapstruct.*;
+
+import java.util.List;
+
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
+public interface UserMapper {
+    @Mapping(source = "id", target = "userId")
+    @Mapping(source = "role.type", target = "roleType")
+    LoginResponse toDto(User user);
+
+
+
+}
