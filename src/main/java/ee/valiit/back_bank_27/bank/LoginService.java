ed2034cc04f9da91043a32116d3983de854d3888
Heda <hedakalmus@gmail.com>
2023-01-30 15:16:10 +0200

    userMapper

--- src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
@@ -1,10 +1,11 @@
 package ee.valiit.back_bank_27.bank;
 
 import ee.valiit.back_bank_27.domain.user.User;
-import ee.valiit.back_bank_27.domain.user.UserMapperImpl;
+import ee.valiit.back_bank_27.domain.user.UserMapper;
 import ee.valiit.back_bank_27.domain.user.UserService;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class LoginService {
@@ -13,15 +14,12 @@ public class LoginService {
     private UserService userService;
 
     @Resource
-    private UserMapperImpl userMapper;
+    private UserMapper userMapper;
 
 
     public LoginResponse login(String username, String password) {
         User user = userService.findUser(username, password);
-
         LoginResponse response = userMapper.toDto(user);
-
-
         return response;
     }
 }
