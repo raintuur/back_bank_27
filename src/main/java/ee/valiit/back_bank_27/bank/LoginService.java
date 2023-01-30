804c1b6abbc0b0df036d40b610f447c8810da9b9
Kuldar Nei <kuldarnei@gmail.com>
2023-01-30 12:33:10 +0200

    BEB27- jätkame backiga. interface, impl, neli kihti, much wow :D

--- src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
@@ -1,12 +1,28 @@
 package ee.valiit.back_bank_27.bank;
 
+import ee.valiit.back_bank_27.domain.user.User;
+import ee.valiit.back_bank_27.domain.user.UserMapper;
+import ee.valiit.back_bank_27.domain.user.UserMapperImpl;
+import ee.valiit.back_bank_27.domain.user.UserService;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
+    @Resource
+    private UserService userService;
+    @Resource
+    private UserMapperImpl userMapper;
+
     public LoginResponse login(String username, String password) {
-        // to be imlpemented
+
+        User user = userService.findUser(username, password);
+
+        LoginResponse response = userMapper.toDto(user);
 
 
-
-        return null;
+        return response;
     }
+
 }
---
+++ src/main/java/ee/valiit/back_bank_27/domain/user/role/Role.java
@@ -0,0 +1,27 @@
+package ee.valiit.back_bank_27.domain.user.role;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Size(max = 50)
+    @NotNull
+    @Column(name = "type", nullable = false, length = 50)
+    private String type;
+
+}
