a6db92019e482eeab6f448b72ba9a6f2b2f263c9
Helar Kärbo <helar.karbo@gmail.com>
2023-01-30 10:12:22 +0200

    midagi

--- src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
+++ src/main/java/ee/valiit/back_bank_27/bank/LoginService.java
@@ -1,11 +1,12 @@
 package ee.valiit.back_bank_27.bank;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class LoginService {
 
     public LoginResponse login(String username, String password) {
         // to be imlpemented
-
-
 
         return null;
     }
