78abca2c0310bdae8f41e7313ff28f2728301e65
Janeli <janelikurm@gmail.com>
2023-01-31 12:04:52 +0200

    Error message

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
@@ -1,7 +1,12 @@
 package ee.valiit.back_bank_27.domain.user;
 
+import ee.valiit.back_bank_27.infrastructure.exception.DataNotFoundException;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+import static ee.valiit.back_bank_27.infrastructure.error.ErrorMessage.INCORRECT_CREDENTIALS;
 
 @Service
 public class UserService {
@@ -10,7 +15,14 @@ public class UserService {
     private UserRepository userRepository;
 
     public User findUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        Optional<User> optionalUser = userRepository.findByUsernameAndPassword(username, password);
+        if (optionalUser.isEmpty()) {
+            throw new DataNotFoundException(INCORRECT_CREDENTIALS.getMessage(), INCORRECT_CREDENTIALS.getCode());
+        }
+
+
+
+        User user = optionalUser.get();
         return user;
     }
 
