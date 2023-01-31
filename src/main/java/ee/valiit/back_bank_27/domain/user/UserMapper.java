41597d165da1e7d0d50fc34e4d2625e1f85efff0
indrekjaadla <indrekjaadla@gmail.com>
2023-01-31 09:04:41 +0200

    Töötav kood.

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserMapper.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserMapper.java
@@ -1,6 +1,6 @@
 package ee.valiit.back_bank_27.domain.user;
 
-import ee.valiit.back_bank_27.bank.LoginResponse;
+import ee.valiit.back_bank_27.bank.login.LoginResponse;
 import org.mapstruct.*;
 
 import java.util.List;
--- src/main/java/ee/valiit/back_bank_27/domain/user/UserRepository.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserRepository.java
@@ -6,6 +6,4 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
     User findByUsernameAndPassword(String username, String password);
 
-
-
 }
