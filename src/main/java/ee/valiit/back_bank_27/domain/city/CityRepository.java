c3e62397a4acb94b701d1da4c232aeb3cb082eb7
katrila <katrilattu@gmail.com>
2023-01-31 09:31:32 +0200

    Added DB stuff

--- src/main/java/ee/valiit/back_bank_27/domain/city/CityRepository.java
+++
@@ -1,8 +0,0 @@
-package ee.valiit.back_bank_27.domain.city;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CityRepository extends JpaRepository<City, Integer> {
-
-}
