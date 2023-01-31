df5f2d3ef616429418af403e415ad8f5d1cc31fa
pbernotas <priit.bernotas@gmail.com>
2023-01-31 10:43:26 +0200

    JAVA WIP

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
@@ -1,7 +1,10 @@
 package ee.valiit.back_bank_27.domain.user;
 
+import ee.valiit.back_bank_27.infrastructure.exception.DataNotFoundException;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -10,7 +13,11 @@ public class UserService {
     private UserRepository userRepository;
 
     public User findUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
+        Optional<User> optionalUser = userRepository.findByUsernameAndPassword(username, password);
+        if (optionalUser.isEmpty()) {
+            throw new DataNotFoundException("Vale kasutajanimi voi parool", "666");
+        }
+        User user = optionalUser.get();
         return user;
     }
 
