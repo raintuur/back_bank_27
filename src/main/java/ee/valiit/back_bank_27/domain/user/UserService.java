8b44f09d2c0e68ecab1a5aa8f26bcd48cc4ebbda
indrekjaadla <indrekjaadla@gmail.com>
2023-01-31 11:06:10 +0200

    Validator.

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
@@ -1,12 +1,13 @@
 package ee.valiit.back_bank_27.domain.user;
 
 import ee.valiit.back_bank_27.infrastructure.exception.DataNotFoundException;
+import ee.valiit.back_bank_27.validation.Validator;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
-import static ee.valiit.back_bank_27.infrastructure.error.ErrorMessage.INCORRECT_CREDENTIALS;
+import static ee.valiit.back_bank_27.validation.ErrorMessage.INCORRECT_CREDENTIALS;
 
 @Service
 public class UserService {
@@ -15,13 +16,8 @@ public class UserService {
     private UserRepository userRepository;
 
     public User findUser(String username, String password) {
-
         Optional<User> optinalUser = userRepository.findByUsernameAndPassword(username, password);
-
-        if (optinalUser.isEmpty()) {
-            throw new DataNotFoundException(INCORRECT_CREDENTIALS.getMessage(), INCORRECT_CREDENTIALS.getCode());
-        }
-
-        return optinalUser.get();
+        User user = Validator.getValidUser(optinalUser);
+        return user;
     }
 }
