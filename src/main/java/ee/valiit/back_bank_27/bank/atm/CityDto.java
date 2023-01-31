22d06c45c9de0924728171dfbcc3161ffaa5805c
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-01-31 09:23:42 +0200

    back stuff

---
+++ src/main/java/ee/valiit/back_bank_27/bank/atm/CityDto.java
@@ -0,0 +1,19 @@
+package ee.valiit.back_bank_27.bank.atm;
+
+import ee.valiit.back_bank_27.domain.city.City;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * A DTO for the {@link City} entity
+ */
+@Data
+public class CityDto implements Serializable {
+    private final Integer cityId;
+    @Size(max = 255)
+    @NotNull
+    private final String cityName;
+}
