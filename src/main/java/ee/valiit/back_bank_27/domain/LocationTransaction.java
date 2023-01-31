483aff4755c125c521f551ffe840d45ae75057cd
Rain Tüür <rain.tuur@gmail.com>
2023-01-31 14:35:09 +0200

    stuff

---
+++ src/main/java/ee/valiit/back_bank_27/domain/LocationTransaction.java
@@ -0,0 +1,31 @@
+package ee.valiit.back_bank_27.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "location_transaction")
+public class LocationTransaction {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "location_id", nullable = false)
+    private Location location;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "transaction_id", nullable = false)
+    private Transaction transaction;
+
+    @NotNull
+    @Column(name = "available", nullable = false)
+    private Boolean available = false;
+
+}
