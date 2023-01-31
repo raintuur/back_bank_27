c95fee6ca49b09ac28c27a94d9eca1dd8bc5e936
Rain Tüür <rain.tuur@gmail.com>
2023-01-31 11:30:35 +0200

    stuff

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
@@ -1,10 +1,14 @@
 package ee.valiit.back_bank_27.domain.user;
 
+import ee.valiit.back_bank_27.bank.login.LoginService;
 import ee.valiit.back_bank_27.infrastructure.exception.DataNotFoundException;
+import ee.valiit.back_bank_27.validation.Validator;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static ee.valiit.back_bank_27.validation.ErrorMessage.INCORRECT_CREDENTIALS;
 
 @Service
 public class UserService {
@@ -14,11 +18,7 @@ public class UserService {
 
     public User findUser(String username, String password) {
         Optional<User> optionalUser = userRepository.findByUsernameAndPassword(username, password);
-        if (optionalUser.isEmpty()) {
-            throw new DataNotFoundException("Vale kasutajanimi või parool", "666");
-        }
-
-        User user = optionalUser.get();
+        User user = Validator.getValidUser(optionalUser);
         return user;
     }
 
