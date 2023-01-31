bc0a1256db24d60664a0b8955706c8a925be3933
Heda <hedakalmus@gmail.com>
2023-01-31 10:44:05 +0200

    errorMessage enum

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserRepository.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserRepository.java
@@ -2,7 +2,11 @@ package ee.valiit.back_bank_27.domain.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsernameAndPassword(String username, String password);
+    Optional<User> findByUsernameAndPassword(String username, String password);
+
+
 
 }
--- src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
@@ -1,7 +1,11 @@
 package ee.valiit.back_bank_27.domain.user;
 
+import ee.valiit.back_bank_27.infrastructure.error.ErrorMessage;
+import ee.valiit.back_bank_27.infrastructure.exception.DataNotFoundException;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -10,8 +14,11 @@ public class UserService {
     private UserRepository userRepository;
 
     public User findUser(String username, String password) {
-        User user = userRepository.findByUsernameAndPassword(username, password);
-
+        Optional<User> optionalUser = userRepository.findByUsernameAndPassword(username, password);
+        if(optionalUser.isEmpty()) {
+            throw new DataNotFoundException();
+        }
+        User user = optionalUser.get();
         return user;
     }
 
