612c4bb53b12c49eb3ce7c95faf363aef942e53a
Mark Saaremägi <mark.saaremagi@gmail.com>
2023-02-01 16:45:08 +0200

    atm location info värk

--- src/main/java/ee/valiit/back_bank_27/domain/location/LocationService.java
+++ src/main/java/ee/valiit/back_bank_27/domain/location/LocationService.java
@@ -1,11 +1,14 @@
 package ee.valiit.back_bank_27.domain.location;
 
+import ee.valiit.back_bank_27.bank.Status;
 import ee.valiit.back_bank_27.validation.Validator;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
-import java.util.IllegalFormatCodePointException;
 import java.util.List;
+import java.util.Optional;
+
+import static ee.valiit.back_bank_27.bank.Status.ACTIVE;
 
 @Service
 public class LocationService {
@@ -13,7 +16,7 @@ public class LocationService {
     private LocationRepository locationRepository;
 
     public List<Location> findActiveLocations(Integer cityId) {
-        List<Location> locations = locationRepository.findLocations(cityId, "A");
+        List<Location> locations = locationRepository.findLocations(cityId, ACTIVE);
         Validator.validateAtmLocationsAvailable(locations);
         return locations;
     }
@@ -21,8 +24,18 @@ public class LocationService {
 
 
     public List<Location> findActiveLocations() {
-        List<Location> locations = locationRepository.findLocations("A");
+        List<Location> locations = locationRepository.findLocations(ACTIVE);
         Validator.validateAtmLocationsAvailable(locations);
         return locations;
+    }
+
+    public Location findLocation(Integer locationId) {
+//        Optional<Location> optionalLocation = locationRepository.findById(locationId);
+//        Location location = optionalLocation.get(); return location;  ------ pikalt lahti kirjutatud lahendus
+        return locationRepository.findById(locationId).get();
+    }
+
+    public void saveAtmLocation(Location location) {
+        locationRepository.save(location);
     }
 }
