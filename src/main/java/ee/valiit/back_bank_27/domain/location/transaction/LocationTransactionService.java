d20ee9c209e42b35aba60e2826da7db4ebdb9420
Rain Tüür <rain.tuur@gmail.com>
2023-02-01 10:23:38 +0200

    stuff

---
+++ src/main/java/ee/valiit/back_bank_27/domain/location/transaction/LocationTransactionService.java
@@ -0,0 +1,19 @@
+package ee.valiit.back_bank_27.domain.location.transaction;
+
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class LocationTransactionService {
+
+    @Resource
+    private LocationTransactionRepository locationTransactionRepository;
+
+
+    List<LocationTransaction> findLocationTransactions(Integer locationId, Boolean isAvailable) {
+        return locationTransactionRepository.findLocationTransactions(locationId, isAvailable);
+    }
+
+}
