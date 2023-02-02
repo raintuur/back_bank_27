590c6dd655dc46d6cc4a59458687af268ea88486
Heda <hedakalmus@gmail.com>
2023-02-02 16:27:59 +0200

    pilt lisatud asukohavaatesse

---
+++ src/main/java/ee/valiit/back_bank_27/util/PictureUtil.java
@@ -0,0 +1,8 @@
+package ee.valiit.back_bank_27.util;
+
+public class PictureUtil {
+
+    public static String byteArrayToString (byte[] byteArray) {
+        return new String(byteArray);
+    }
+}
