55157a6245379b09b17be6333f32457ec852d0cf
Heda <hedakalmus@gmail.com>
2023-02-02 15:23:25 +0200

    clusterfuck

--- src/main/java/ee/valiit/back_bank_27/domain/locationtransaction/location/LocationMapper.java
+++ src/main/java/ee/valiit/back_bank_27/domain/locationtransaction/location/LocationMapper.java
@@ -2,7 +2,7 @@ package ee.valiit.back_bank_27.domain.locationtransaction.location;
 
 import ee.valiit.back_bank_27.bank.Status;
 import ee.valiit.back_bank_27.bank.atm.dto.AtmLocationDto;
-import ee.valiit.back_bank_27.bank.atm.dto.AtmLocationInfo;
+import ee.valiit.back_bank_27.bank.atm.dto.AtmLocationResponse;
 import org.mapstruct.*;
 
 import java.nio.charset.StandardCharsets;
@@ -14,7 +14,7 @@ public interface LocationMapper {
     @Mapping(source = "locationName", target = "name")
     @Mapping(constant = Status.ACTIVE, target = "status")
     @Mapping(source = "picture", target = "picture", qualifiedByName = "stringToByteArray")
-    Location toEntity(AtmLocationInfo atmLocationInfo);
+    Location toEntity(AtmLocationDto atmLocationDto);
 
     @Named("stringToByteArray")
     static byte[] stringToByteArray(String picture) {
@@ -26,15 +26,15 @@ public interface LocationMapper {
     @Mapping(source = "id", target = "locationId")
     @Mapping(source = "name", target = "locationName")
     @Mapping(source = "city.name", target = "cityName")
-    AtmLocationDto toDto(Location location);
+    AtmLocationResponse toDto(Location location);
 
 
     @Mapping(source = "name", target = "locationName")
     @Mapping(source = "city.id", target = "cityId")
     @Mapping(ignore = true, target = "picture")
-    AtmLocationInfo toInfo(Location location);
+    AtmLocationDto toInfo(Location location);
 
 
-    List<AtmLocationDto> toDtos(List<Location> locations);
+    List<AtmLocationResponse> toDtos(List<Location> locations);
 
 }
