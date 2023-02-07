bb4eecfeff67498961aa9267fc3d8768d959d00b
Rain Tüür <rain.tuur@gmail.com>
2023-02-07 10:22:35 +0200

    RAIN FINAL

--- src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
+++ src/main/java/ee/valiit/back_bank_27/domain/user/UserService.java
@@ -7,15 +7,49 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+// @Service annotatsioon annab Springile märku, et see klass on teenuse tüüpi klass.
+// Tänu sellele annotatsioonile teeb Spring rakendust käivitades sellest klassist ühe objekti.
+// @Resource abil saame hiljem sellele objektile ligi.
+// Selle klassi objekti kaudu saame käivitada kõiki siin klassis olevaid PUBLIC meetodeid.
 @Service
 public class UserService {
 
+    // @Resource abiga saab Spring teha meile ligipääsu:
+    // @Service, @Component, @Mapper, @Repository, jne annotatsiooniga klasside objektidele (beans)
     @Resource
     private UserRepository userRepository;
 
+    // See meetod on defineeritud nii, et ta võtab sisse String tüüpi objektid
+    // See on selleks vajalik, et me saaksime siin meetodis nende objektide andmetega kuidagi edasi toimetada.
+    // See meetod on defineeritud nii, et ta peab tagastama User tüüpi objekti (andmebaasi tabeli Entity)
+    // Kui meetodite teema on veel endiselt segane, siis palun vaata uuesti "Meetodid", "Meetodite signatuurid" ja "Public ja Private meetodid":
+    // https://youtu.be/EI3XfkdPBc4
+    // https://youtu.be/GvP68LBZiUA
+    // https://youtu.be/4ZkvNfu9kNw
     public User findUser(String username, String password) {
+
+        // kasutame userRepository (värav) objekti, et saada ligi tabeli 'user' andmetele
+        // See meetod findUser() on meie enda loodud meetod.
+        // Kaasa anname:
+        //      konkreetse kasutaja kasutajanime ehk 'user' tabeli veeru 'username' väärtuse.
+        //      konkreetse kasutaja parooli ehk 'user' tabeli veeru 'password' väärtuse.
+        //      konkreetse aktiivse statuse ehk 'user' tabeli veeru 'status' väärtuse "A".
+        // repository tagastab nendele tingimustele vastava 'user' objekti (andmebaasi mõttes tagastatakse 'user' tabeli andmetega rida)
+        // Tulemus mähitakse omakorda "Optional" klassi objekti sisse (komm ja kommipaber)
+        // Sellel Optional klassil on olemas mugavad meetodid, millega saab kontrollida, et kas leiti mingi tulemus (user objekt pole 'null'
+        // või siis ei leitud mingit tulemust.
         Optional<User> optionalUser = userRepository.findUser(username, password, Status.ACTIVE);
+
+        // Kutsume välja Validator klassi staatilise meetodi getValidUser().
+        // Pane tähele, siis ei ole pöördutud mingi klassi OBJEKTI poole vaid on pöördutud OTSE klassi enda poole
+        // (Validator algab siin suure tähega, see ei ole mingi objekt)
+        // Kaasa anname eelnevalt leitud 'optionalUser' objekti
+        // vaata ka kommentaare selle 'Validator.getValidUser()' meetodi sees
+        // Kui getValidUser()'st visatakse ülesse viga, siis siit allapoole kood enam edasi ei lähe
         User user = Validator.getValidUser(optionalUser);
+
+
+        // Kui me validatsioonis viga ei saanud, RETURN'iga tagastatakse tulemus sinna kohta, kust see meetod välja kutsutakse
         return user;
     }
 
--- src/main/java/ee/valiit/back_bank_27/validation/Validator.java
+++ src/main/java/ee/valiit/back_bank_27/validation/Validator.java
@@ -12,11 +12,39 @@ import static ee.valiit.back_bank_27.validation.ErrorMessage.NO_ATM_LOCATIONS;
 
 public class Validator {
 
+    // See meetod on defineeritud staatilise meetodina. See tähendab seda, et seda meetodit on võimalik välja kutsuda
+    // ilma, et peaks enne sellest klassist tegema objekti.
+    // Staatilist meetodit saab välja kutsuda kirjutades välja klassi nime ja siis meetodi nime (Validator.getValidUser())
+    // Eelduseks on see, et siin meetodis ei kasutada kuidagi klassi instantsi muutujaid näiteks:
+    //    public class Validator {
+    //        private String name;
+    //        private int age;
+    //    }
+    // Sest kui me kutsuksime selle meetodi klassi nime abil staatiliselt välja, siis sellest klassist me ju objekti ei tee ning neid välju ei väärtusta
+    // Seega jääks selle klassi intsantsi muutujate väärtused null'ideks ning nendega ei oleks siis võimaluk kuidagi toimetada.
+    // See meetod on defineeritud nii, et ta võtab sisse Optional<User> tüüpi 'optionalUser' objekti
+
     public static User getValidUser(Optional<User> optionalUser) {
+        // Võtame getValidUser() signatuuri parameetris sisse Optional<User> tüüpi objekti
+        // Siin signatuuris antakse selle objekti muutuja nimeks 'optionalUser'
+
+        // optionalUser objekti klassi  tüüp on Optional<User>
+        // Siin 'if' lause sees kasutatakse Optional klassi meetodit isEmpty(),
+        // mis tagastab 'true' kui selle Optional klassi sisse mähitud objekti väärtus on 'null'
         if (optionalUser.isEmpty()) {
+            // Kui if lause sisu rehtkendus true-ks, siis tuldi siia sisse, et visata süsteemis ülesse viga
+            // vigu saame visata ülesse kasutades võtme sõnu "throw new ..."
+            // vaata ka nende klasside DataNotFoundException ja BusinessException sisse
             throw new DataNotFoundException(INCORRECT_CREDENTIALS.getMessage(), INCORRECT_CREDENTIALS.getCode());
         }
+
+        // Kui me eelneva 'if' lause sisse ei satu, kus vistakase viga ja lahkutakse siit meetodist,
+        // siis järgmiseks satume siin all olevale koodireale
+        // Siin siis võtame Optional klassi meetodiga .get() optional objekti sisse mähitud 'user' objekti välja
+        // ja paneme muutujasse user  (komm ja kommipaber)
         User user = optionalUser.get();
+
+        // RETURN'iga tagastatakse tulemus sinna kohta, kust see meetod välja kutsutakse
         return user;
     }
 
