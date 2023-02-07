65312d4f9c79c8f05cdc9b53c8446a36c057c1e1
Rain Tüür <rain.tuur@gmail.com>
2023-02-07 10:51:10 +0200

    RAIN FINAL

--- src/main/java/ee/valiit/back_bank_27/bank/Status.java
+++ src/main/java/ee/valiit/back_bank_27/bank/Status.java
@@ -4,9 +4,4 @@ public class Status {
     public static final String DEACTIVATED = "D";
     public static final String ACTIVE = "A";
 
-
-    public static String getCoolName(String name) {
-        return name + " is cool name";
-    }
-
 }
